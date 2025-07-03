89782ac6477bc728a8cb30a582dd5c32499fafc9
Jacob <jjcarrano@gmail.com>
2025-07-03 16:01:06 -0700

    expanded pointers module

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,5 +1,6 @@
 param(
-    [string]$Preset  = "Debug"
+    [string]$Preset  = "Debug",
+    [string]$Run = ""
 )
 
 $ErrorActionPreference = "Stop"
@@ -14,3 +15,13 @@ $cmake = Join-Path $cmakeDir 'cmake*\bin\cmake.exe'
 
 # Build using the chosen preset
 & $cmake --build  --preset $Preset
+
+# Run the executable if provided
+if ($Run -ne "") {
+    # Try to find the executable path relative to known output directories
+    $buildDir = ".\build\default\$Preset"
+    $exePath = Join-Path $buildDir $Run
+
+    Write-Host "Running executable: $exePath"
+    & $exePath
+}
--- src/pointers.cpp
+++ src/pointers.cpp
@@ -6,4 +6,9 @@ void pointerDemo() {
     int* p = &x; // Pointer to x
     fmt::println("Value of x: {}", x);
     fmt::println("Address of x: {}", static_cast<void*>(&x));
+    fmt::println("Value of p: {}", static_cast<void*>(p)); // same as &x
+    fmt::println("Value pointed to by p: {}", *p); // same as x
+
+    *p = 100; // Change the value of x through the pointer
+    fmt::println("New value of x: {}", x);
 }
